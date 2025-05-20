ac5ecd5ef535cfee78d3c47eebe08c56f31108b1
Muhammed Firat <Firat@ms15.virtual-office.de>
2025-05-20 13:28:33 +0200

    chore: prepare for Angular update

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter }       from '@angular/router';
-import { provideAnimations }   from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
-import { MatToolbarModule }    from '@angular/material/toolbar';
-import { MatMenuModule }       from '@angular/material/menu';
-import { MatButtonModule }     from '@angular/material/button';
-import { MatIconModule }       from '@angular/material/icon';
-import { RouterModule }        from '@angular/router';
-import { AppComponent }        from './app/app.component';
-import { routes }              from './app/app.routes';
+// src/main.ts
+import { bootstrapApplication }    from '@angular/platform-browser';
+import { importProvidersFrom }     from '@angular/core';
+import { BrowserModule }           from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter }           from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { routes }       from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    provideAnimations(),
     importProvidersFrom(
-      RouterModule,
-      MatToolbarModule,
-      MatMenuModule,
-      MatButtonModule,
-      MatIconModule
-    )
+      BrowserModule,
+      BrowserAnimationsModule
+    ),
+    provideRouter(routes)
   ]
 })
 .catch(err => console.error(err));
